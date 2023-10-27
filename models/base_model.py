8418d6bf7eb91c42f94fc4f4af4c7ea41e5008d7
ROgbuagu05 <chizzy3003@gmail.com>
2023-10-27 06:35:56 -0700

    Task 13

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,6 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm.properties import RelationshipProperty
 import uuid
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
@@ -36,16 +35,16 @@ class BaseModel:
             if kwargs.get("created_at", None) and type(self.created_at) is str:
                 self.created_at = datetime.strptime(kwargs["created_at"], time)
             else:
-                self.created_at = datetime.utcnow()
+                self.created_at = datetime.now()
             if kwargs.get("updated_at", None) and type(self.updated_at) is str:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], time)
             else:
-                self.updated_at = datetime.utcnow()
+                self.updated_at = datetime.now()
             if kwargs.get("id", None) is None:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
+            self.created_at = datetime.now()
             self.updated_at = self.created_at
 
     def __str__(self):
@@ -59,26 +58,17 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self, internal=False):
+    def to_dict(self):
         """returns a dictionary containing all keys/values of the instance"""
-        ret = {}
-        for key, value in self.__dict__.items():
-            if key == 'created_at':
-                ret['created_at'] = value.strftime(time)
-            elif key == 'updated_at':
-                ret['updated_at'] = value.strftime(time)
-            elif key == 'password' and not internal:
-                continue
-            elif key == '_sa_instance_state':
-                continue
-            elif not (
-                hasattr(type(self), key) and
-                hasattr(getattr(type(self), key), 'prop') and
-                isinstance(getattr(type(self), key).prop, RelationshipProperty)
-            ):
-                ret[key] = value
-        ret['__class__'] = type(self).__name__
-        return ret
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+        return new_dict
 
     def delete(self):
         """delete the current instance from the storage"""
