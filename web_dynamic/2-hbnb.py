194635a733f55d30c42d5d6664ed5cf0d06c392c
ROgbuagu05 <chizzy3003@gmail.com>
2023-11-02 04:06:34 -0700

    Task 3 and 4

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -2,10 +2,9 @@
 """
 A Flask Web Application
 """
+import uuid
 from flask import Flask, render_template, url_for
 from models import storage
-import uuid
-
 
 # flask setup
 app = Flask(__name__)
@@ -24,7 +23,7 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.route('/1-hbnb/')
+@app.route('/2-hbnb/')
 def hbnb_filters(the_id=None):
     """
     handles request to custom template with states, cities & amentities
@@ -35,11 +34,14 @@ def hbnb_filters(the_id=None):
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
-    return render_template('1-hbnb.html',
+    cache_id = (str(uuid.uuid4()))
+    return render_template('2-hbnb.html',
                            states=states,
                            amens=amens,
                            places=places,
-                           users=users, cache_id=uuid.uuid4())
+                           users=users,
+                           cache_id=cache_id)
+
 
 if __name__ == "__main__":
     """
